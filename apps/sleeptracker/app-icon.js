3fecc0338fffe4e529a1e40b0863b1eb3499b8d2
Prem Shinde <74344694+Kugelblitz-26@users.noreply.github.com>
2025-07-04 13:22:18 +0200

    Create app-icon.js

---
+++ apps/sleeptracker/app-icon.js
@@ -0,0 +1,4 @@
+// Renamed to app-icon.js for the App Loader
+// Original: sleeplog.img (in code form)
+// A simple blue moon icon
+require("heatshrink").decompress(atob("mEwwICYfwA/AH4A/AH4A/AFoA/AFoA/AFoA/AGwA/AGwA/AGwA/AHAEOAHAEOAHAEOAHAGeAHAEOAHAHWAL8A/AFgA/AFgA/AFgA/AH4A/AH4A/AH4A/ADg="));
